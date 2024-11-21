07f2490e565f49220428c3888673494aed299ce1
W.L.A.D.D.A. Liyanaarachchi <139705037+anuradhya@users.noreply.github.com>
2024-11-21 15:59:04 +0000

    created Childcom.js

---
+++ myapp/src/Childcom.js
@@ -0,0 +1,10 @@
+export default function Childcom(props){
+    return(
+        <>
+        <h3>Hello React I'm Childcom</h3>
+        {
+            props.setfun("My name is Anuradhya")
+        }
+        </>
+    )
+}
